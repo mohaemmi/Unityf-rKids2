3bf8687278f7fb2b6cc5a626220ab4dededf687b
mohaemmi <115582940+mohaemmi@users.noreply.github.com>
2022-11-05 15:53:13 +0100

    -

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -18,19 +18,19 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * Speed);
+            Figur.Move(Vector3.left * Time.deltaTime * Speed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * Speed);
+            Figur.Move(Vector3.right * Time.deltaTime * Speed);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * Speed);
+            Figur.Move(Vector3.up * Time.deltaTime * Speed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.down * Time.deltaTime * Speed);
+            Figur.Move(Vector3.down * Time.deltaTime * Speed);
         }
     }
 }
