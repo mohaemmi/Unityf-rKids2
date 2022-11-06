49ed128c2a2706482482aa01f0ce6c5f0405b781
mohaemmi <115582940+mohaemmi@users.noreply.github.com>
2022-11-06 16:54:35 +0100

    -

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,33 +4,36 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private CharacterController Figur;
-    public float Speed;
+    public float jump = 5.0f;
+    public float gravity = 15.0f;
+    public float speed = 5.0f;
+    public Sprite front, back;
+    public Sprite right1, right2;
+    public Sprite left1, left2;
+
+    private SpriteRenderer spriteRenderer;
+    private CharacterController figur;
+    private Vector3 moveVector = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        Figur = GetComponent<CharacterController>();
+        figur = GetComponent<CharacterController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Figur.Move(Vector3.left * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Figur.Move(Vector3.right * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            Figur.Move(Vector3.up * Time.deltaTime * Speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            Figur.Move(Vector3.down * Time.deltaTime * Speed);
-        }
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y -= gravity * Time.deltaTime;
+
+        if (Input.GetButton("Jump")) moveVector.y = jump;
+
+        if (moveVector.x < 0) spriteRenderer.sprite = left1;
+        else if (moveVector.x > 0) spriteRenderer.sprite = right1;
+        else spriteRenderer.sprite = front;
+
+        figur.Move(moveVector * Time.deltaTime * speed);
     }
 }
